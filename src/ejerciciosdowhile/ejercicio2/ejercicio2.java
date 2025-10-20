e9df4e966d33281abfd1314de081cd4e2f87d9f7
Djalminhagod <r.senra@liceolapaz.net>
2025-10-20 11:51:55 +0200

    ejerciciowhile2 terminado

--- src/ejerciciosdowhile/ejercicio2/ejercicio2.java
+++ src/ejerciciosdowhile/ejercicio2/ejercicio2.java
@@ -10,25 +10,26 @@ public class ejercicio2 {
         System.out.println("******************************************");
         System.out.println("******************************************");
         Scanner sc = new Scanner(System.in);
+        boolean salir = true;
         int numero=1000;
         do {
             System.out.println("1.Consultar saldo");
             System.out.println("2.Ingresar dinero");
             System.out.println("3.Retirar dinero");
             System.out.println("4.Salir");
-            String opcion = sc.nextLine();
+            int opcion = sc.nextInt();
             switch (opcion) {
-                case "1":
+                case 1:
                     System.out.println("Tienes "+numero+" €");
                     break;
-                case "2":
+                case 2:
                     System.out.println("Cuanto dinero quieres ingresar?");
                     int dinero = sc.nextInt();
                     numero=numero+dinero;
                     System.out.println("ingresas "+dinero);
                     System.out.println("tendrias saldo "+numero);
                     break;
-                case "3":
+                case 3:
                     System.out.println("cuanto quieres retirar?");
                     boolean retiradainvalida = true;
                     int retira = sc.nextInt();
@@ -37,22 +38,29 @@ public class ejercicio2 {
                         while (retiradainvalida) {
                             System.out.println("cuanto quieres retirar?");
                             retira = sc.nextInt();
-                            if(retira<numero || retira>0) {
+                            if(retira>numero || retira<=0) {
+                                System.out.println("numero no valido");
+                            }
+                            else {
                                 retiradainvalida = false;
+                                numero=numero-retira;
+                                System.out.println("retiras "+retira);
+                                System.out.println("tendrias saldo "+numero);
                             }
                         }
                     }else{
                         numero=numero-retira;
+                        System.out.println("retiras "+retira);
+                        System.out.println("tendrias saldo "+numero);
                     }
-                    System.out.println("retiras "+retira);
-                    System.out.println("tendrias saldo "+numero);
                     break;
-                case "4":
+                case 4:
                     System.out.println("sales");
+                    salir = false;
                     break;
             }
-            System.out.println("tendrias saldo "+numero);
 
-        }while (true);
+
+        }while (salir);
     }
 }
