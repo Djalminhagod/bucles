0f1e6b16cfb6ba1a5f20be59e0161cf7c4a62061
Djalminhagod <r.senra@liceolapaz.net>
2025-10-22 13:13:12 +0200

    ejercicio3 terminado

--- src/ejerciciosdowhile/ejercicio3/ejercicio3.java
+++ src/ejerciciosdowhile/ejercicio3/ejercicio3.java
@@ -6,6 +6,7 @@ public class ejercicio3 {
     public static void main(String[] args) {
         boolean haganado = false;
         boolean haperdido = false;
+        int codigoram = (int) (Math.random() * 10) + 1;
         Scanner sc = new Scanner(System.in);
         System.out.println("te levantas en una habitacion vacia ");
         System.out.println("en la habiatcion se veria una mesita, un armario y una puerta ");
@@ -13,7 +14,8 @@ public class ejercicio3 {
             System.out.println("1.Abro el cajon");
             System.out.println("2.Abro el armario");
             System.out.println("3.Abro el puerta");
-            int llave;
+            int llave1 =1;
+            int llave = 0;
             int opcion= sc.nextInt();
             switch (opcion){
                 case 1:
@@ -23,16 +25,20 @@ public class ejercicio3 {
                     System.out.println("en el armario hay una caja fuerte ");
                     System.out.println("que hacer?? lo intentas abrir o vuelves atras");
                     String cajafuerte = sc.next();
-                    if(cajafuerte.equals("abri")){
+                    if(cajafuerte.equals("abrir")){
                         for (int i = 5 ; i>=0; i--) {
                             System.out.println("Intruce codigo numerico del 1 al 10");
                             int codigo = sc.nextInt();
-                            int codigoram = (int) (Math.random() * 10) + 1;
                             if (codigoram == codigo) {
                                 System.out.println("Codigo correcto hay una llave ");
                                 llave = 1;
+                                System.out.println("Quieres abrir la puerte?? ");
+                                System.out.println("inserta la llave");
+                                System.out.println("has ganado escapaste");
+                                haganado = true;
+                                break;
                             } else {
-                                System.out.println("Codigo no correcto ");
+                                System.out.println("Codigo no correcto "+codigoram);
                             }
                             if (i==0){
                                 System.out.println("se bloque la caja fuerte");
@@ -53,12 +59,19 @@ public class ejercicio3 {
                     switch (puerta){
                         case 1:
                             int golpearpuerta = (int)(Math.random() *100) +1;
+                            System.out.println("introduce un numero del 1 al 100 para romper la puerta ");
                             int puertapuerta = sc.nextInt();
                             if(golpearpuerta == puertapuerta){
                                 System.out.println("Abriste la puerta ");
                                 System.out.println("Has Ganado");
                             }else {
                                 System.out.println("No se abrio");
+                            }
+                            break;
+                        case 2:
+                            System.out.println("inserta la llave");
+                            if (llave == 0){
+                                System.out.println("que haces??? no tienes la llave busca rapidooo!!!!!");
                             }
                     }
             }
